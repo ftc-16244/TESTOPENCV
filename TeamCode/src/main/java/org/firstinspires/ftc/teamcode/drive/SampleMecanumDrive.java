131df7ed0a1575eaa9b7c498f15a40251e580ccd
David Atkinon <david.atkinson@netzero.com>
2021-10-17 09:57:39 -0500

    added two wheel localizer to test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -53,10 +53,10 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(1, 0, 0.0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(1, 0, 0.0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(4, 0, 0.0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(8, 0, 0.0);
 
-    public static double LATERAL_MULTIPLIER = 0.3;// may need to change to 0.2
+    public static double LATERAL_MULTIPLIER = 1.0;// may need to change to 0.2
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
@@ -130,8 +130,8 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         // TODO: if desired, use setLocalizer() to change the localization method
         // for instance, setLocalizer(new ThreeTrackingWheelLocalizer(...));
-        setLocalizer(new StandardTrackingWheelLocalizer(hardwareMap)); // changes localizer to tracking wheels
-
+        // setLocalizer(new StandardTrackingWheelLocalizer(hardwareMap)); // changes localizer to tracking wheels
+        setLocalizer(new TwoWheelTrackingLocalizer(hardwareMap,this)); // two wheel version
         trajectorySequenceRunner = new TrajectorySequenceRunner(follower, HEADING_PID);
     }
 
