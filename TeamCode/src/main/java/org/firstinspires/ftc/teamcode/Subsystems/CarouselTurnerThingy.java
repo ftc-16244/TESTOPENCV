33ccf75922dec23179147c33b9489a3aed3d40ed
David Atkinon <david.atkinson@netzero.com>
2021-10-23 10:39:45 -0500

    added alliance color selection and auto init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CarouselTurnerThingy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CarouselTurnerThingy.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Enums.Alliance;
+
+import static org.firstinspires.ftc.teamcode.Enums.Alliance.BLUE;
 
 public class CarouselTurnerThingy {
 
@@ -12,10 +17,21 @@ public class CarouselTurnerThingy {
     public static final double      CAROUSELOFF = 0;
     public static final double      CAROUSELON = 0.5;
 
-    public void init(HardwareMap hwMap)  {
+    // create a constructor to add in the special requirements
+    public CarouselTurnerThingy(HardwareMap hwMap, Alliance Color) {
+
+
+
+    //public void init(HardwareMap hwMap, Alliance Alliance)  {
         carousel = hwMap.get(DcMotor.class,"carousel");
-        carousel.setDirection(DcMotor.Direction.FORWARD);
         carousel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if(Color == BLUE) {
+            carousel.setDirection(DcMotor.Direction.FORWARD);
+        }
+        else {
+            carousel.setDirection(DcMotor.Direction.REVERSE);
+        }
+
     }
 
     public void carouselTurnOn() {
