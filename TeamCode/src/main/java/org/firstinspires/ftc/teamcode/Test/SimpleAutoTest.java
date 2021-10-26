0015ebfe2cfd9d42b9478422e4899340657d8ee8
David Atkinon <david.atkinson@netzero.com>
2021-10-25 23:37:18 -0500

    initial test files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/SimpleAutoTest.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode.Test;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Enums.LiftPosition;
+import org.firstinspires.ftc.teamcode.Enums.PatrickState;
+import org.firstinspires.ftc.teamcode.Subsystems.CarouselTurnerThingy;
+import org.firstinspires.ftc.teamcode.Subsystems.Felipe;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import static org.firstinspires.ftc.teamcode.Enums.Alliance.BLUE;
+
+/*
+ * This is a simple routine to test translational drive capabilities.
+ */
+@Config
+@Autonomous(group = "Test")
+public class SimpleAutoTest extends LinearOpMode {
+    public static double DISTANCE = 30; // in
+    Felipe felipe = new Felipe(); // instantiate Felipe (the main implement)
+    CarouselTurnerThingy carousel = new CarouselTurnerThingy();
+    // init and setup
+    ElapsedTime runtime = new ElapsedTime();
+
+
+    // ENUMS
+    //DriveSpeedState  currDriveState;
+    PatrickState patrickState = PatrickState.OFF;
+    LiftPosition liftPosition = LiftPosition.DOWN;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+       
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // initialize the other subsystems
+        felipe.init(hardwareMap);
+        carousel.init(hardwareMap, BLUE);
+
+        ///////////////////////////////////////////////////////////////////////////
+        // Trajectories Here
+        ///////////////////////////////////////////////////////////////////////////
+        Trajectory trajectory = drive.trajectoryBuilder(new Pose2d())
+                .forward(DISTANCE)
+                .addTemporalMarker(0.01,()->{
+                    felipe.intakeOn();
+                })
+                .addTemporalMarker(0.50,()->{
+                    felipe.homieLeft();
+                })
+                .addTemporalMarker(2.0,()->{
+                    felipe.intakeOff();
+                })
+
+                .build();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.followTrajectory(trajectory);
+
+        Pose2d poseEstimate = drive.getPoseEstimate();
+        telemetry.addData("finalX", poseEstimate.getX());
+        telemetry.addData("finalY", poseEstimate.getY());
+        telemetry.addData("finalHeading", poseEstimate.getHeading());
+        telemetry.update();
+
+        while (!isStopRequested() && opModeIsActive()) ;
+    }
+}
