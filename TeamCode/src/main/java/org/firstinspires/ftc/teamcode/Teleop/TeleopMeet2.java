11c93e9ed13120412c86a2b736632662c8c3d3c4
Nikita Rudrapati <nikirudrapati@gmail.com>
2021-11-29 19:49:09 -0600

    while loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopMeet2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopMeet2.java
@@ -259,10 +259,10 @@ public class TeleopMeet2 extends LinearOpMode {
 
                 if((juanError < juanErrorMax) && Math.abs(felipe.getJulioPosition()) >= 15) {
 
-                    //felipe.rotateToTargetAngle(0,1);
-                    felipe.setJulioToZero();
-                    felipe.julioArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    felipe.julioArm.setPower(Math.abs(felipe.JULIOTURNSPEED));
+                    felipe.rotateToTargetAngle(0,1);
+                    //felipe.setJulioToZero();
+                    //felipe.julioArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    //felipe.julioArm.setPower(Math.abs(felipe.JULIOTURNSPEED));
 
                     telemetry.addData("Lift State", liftPosition);
                     telemetry.addData("Arm Only Moving . Cunnet degrees", felipe.getJuanPosition());
